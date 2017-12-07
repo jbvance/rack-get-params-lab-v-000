ae46b27a4d3d0c7ea0df582b5d9e29d2d3623b1d
Jason Vance <jbvance@gmail.com>
2017-12-07 14:13:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,9 +16,14 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
+      if !cart.empty?
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
     elsif req.path.match(/add/)
       item = req.params["item"].strip
       if !@@items.include?(item)
